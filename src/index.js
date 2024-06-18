6535b96f693ff6cfb1669e87f34f80bcb8e045e5
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-06-18 10:27:12 +0800

    Fixing code logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import _ from 'lodash'
 import './style.css' 
+import { Player } from './classes/player'
+import { buildPlayerBoardPlacer } from './dom_manipulation/boardCreationPlayer'
+import { buildComputerBoard } from './dom_manipulation/boardCreationComputer';
+import { gameStart } from './dom_manipulation/gameStart';
+
+const startBtn = document.getElementById("start");
+const player = new Player();
+const computer = new Player();
+
+
+buildPlayerBoardPlacer(player)
+startBtn.addEventListener("click", () => {
+    buildComputerBoard(computer)
+    player.switchTurn()
+    gameStart(player, computer)
+})
