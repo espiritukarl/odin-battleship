ef8bd839c76247d29fd07d0953f41c64525b753d
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-06-27 05:26:33 +0800

    Fixed styling for hits and misses

--- src/dom_manipulation/gameStart.js
+++ src/dom_manipulation/gameStart.js
@@ -20,7 +20,6 @@ export function handlePlayerTurn(player, computer) {
             if (!computer.gameboard.receiveAttack([x, y])) {
                 computerBoard.removeEventListener('click', playerClickListener);
                 event.target.classList.add("miss")
-                event.target.textContent = "O"
                 playerBoard.classList.remove("dim")
                 computerBoard.classList.add("dim")
                 results.textContent = "Computer's Turn!"
@@ -49,7 +48,6 @@ function handleComputerTurn(player, computer) {
     try {
         if (!player.gameboard.receiveAttack([x, y])) {
             position.classList.add("miss")
-            position.textContent = "O"
             handlePlayerTurn(player, computer); //swap turn
         } else {
             position.classList.add("hit")
